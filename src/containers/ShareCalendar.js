d4b4e95c9bec3fecfd47fbd15bf7d0b199c23712
linxea <linxea.linxea@gmail.com>
2019-03-31 05:18:25 +0800

    Fix layout in mobile view

--- src/containers/ShareCalendar.js
+++ src/containers/ShareCalendar.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const ShareCalendar = props => (
   <div className="container share-calendar">
-    <h1>Send Calendar to Contact:</h1>
-    <button>Send</button>
+    <h1>Send Calendar:</h1>
+    <button
+      onClick={() => {
+        props.history.push('/dashboard')
+      }}>
+      Send
+    </button>
   </div>
 )
 
