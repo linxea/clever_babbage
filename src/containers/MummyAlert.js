366f22e1bcbd12ceffa138088a7be73ad0242641
Lim <nicole.lim@accenture.com>
2019-03-31 15:12:39 +0800

    update settings popups

--- src/containers/MummyAlert.js
+++ src/containers/MummyAlert.js
@@ -13,6 +13,8 @@ class MummyAlert extends React.Component {
   constructor(props) {
     super(props)
     this.showWarningSnackbar = this.showWarningSnackbar.bind(this)
+    this.showConfirm = this.showConfirm.bind(this)
+    this.showDangerToast = this.showDangerToast.bind(this)
   }
 
   showWarningSnackbar = () => {
@@ -20,11 +22,48 @@ class MummyAlert extends React.Component {
         message: 'Avoid drinking cold drinks',
         color: 'warning'
     });
+    this.showWarningSnackbar2();
+  }
+  
+  showWarningSnackbar2 = () => {
     mobiscroll.snackbar({
-      message: 'Sleep early tonight',
+      message: 'Sleep early starting from tonight',
       color: 'warning'
     });
-}
+  }
+
+  showConfirm = () => {
+    mobiscroll.confirm({
+        title: 'How are you feeling today?',
+        message: 'Having any cramps?',
+        okText: 'Yes',
+        cancelText: 'No',
+    });
+    this.showConfirm2();
+  }
+  showConfirm2 = () => {
+    mobiscroll.confirm({
+        title: 'How are you feeling today?',
+        message: 'How bad is it?',
+        okText: 'Mild',
+        cancelText: 'Severe',
+    });
+    this.showPrimarySnackbar();
+  }
+
+  showPrimarySnackbar = () => {
+    mobiscroll.snackbar({
+        message: 'Hope you feel better soon.',
+        color: 'primary'
+    });
+  }
+
+  showDangerToast = () => {
+    mobiscroll.toast({
+        message: 'Hubby is aware not to annoy you for the next few days.',
+        color: 'danger'
+    });
+  }
 
   render() {
     return (
@@ -34,7 +73,11 @@ class MummyAlert extends React.Component {
                 <mobiscroll.FormGroupTitle>Mummy's alerts</mobiscroll.FormGroupTitle>
                 <div className="mbsc-btn-group-block">
                     <mobiscroll.Button onClick={this.showWarningSnackbar}>No cold drinks</mobiscroll.Button>
+                    <mobiscroll.Button onClick={this.showConfirm}>BF mode</mobiscroll.Button>
+                    <mobiscroll.Button onClick={this.showDangerToast}>Husband mode</mobiscroll.Button>
+                            
                 </div>
+                
             </mobiscroll.FormGroup>
           </mobiscroll.Form>
       </div>
--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -14,44 +14,112 @@ class Settings extends React.Component {
     super(props)
     this.onChange1 = this.onChange1.bind(this)
     this.onChange2 = this.onChange2.bind(this)
+    this.showWarningSnackbar = this.showWarningSnackbar.bind(this)
+    this.showConfirm = this.showConfirm.bind(this)
+    this.showDangerToast = this.showDangerToast.bind(this)
     
     this.state = {
-      switch1: true,
-      switch2: false,
+      mummy: false,
+      bf: false,
+      husband: false,
     };
   }
 
   onChange1 = (event) => {
+    if (event.target.checked) {
+      this.showWarningSnackbar();
+    }
     this.setState({
-        switch1: event.target.checked
+        mummy: event.target.checked
     });
 }
 
   onChange2 = (event) => {
+    if (event.target.checked) {
+      this.showConfirm();
+    }
     this.setState({
-        switch2: event.target.checked
+        bf: event.target.checked
+    });
+  }
+
+  onChange3 = (event) => {
+    if (event.target.checked) {
+      this.showDangerToast();
+    }
+    this.setState({
+        husband: event.target.checked
+    });
+  }
+
+  showWarningSnackbar = () => {
+    mobiscroll.snackbar({
+        message: 'Avoid drinking cold drinks',
+        color: 'warning'
+    });
+    this.showWarningSnackbar2();
+  }
+  
+  showWarningSnackbar2 = () => {
+    mobiscroll.snackbar({
+      message: 'Sleep early starting from tonight',
+      color: 'warning'
+    });
+  }
+
+  showConfirm = () => {
+    mobiscroll.confirm({
+        title: 'How are you feeling today?',
+        message: 'Having any cramps?',
+        okText: 'Yes',
+        cancelText: 'No',
+    });
+    this.showConfirm2();
+  }
+  showConfirm2 = () => {
+    mobiscroll.confirm({
+        title: 'How are you feeling today?',
+        message: 'How bad is it?',
+        okText: 'Mild',
+        cancelText: 'Severe',
+    });
+    this.showPrimarySnackbar();
+  }
+
+  showPrimarySnackbar = () => {
+    mobiscroll.snackbar({
+        message: 'Hope you feel better soon.',
+        color: 'primary'
+    });
+  }
+
+  showDangerToast = () => {
+    mobiscroll.toast({
+        message: 'Hubby is aware not to annoy you for the next few days.',
+        color: 'danger'
     });
   }
 
   render() {
     return (
       <div className="container question">
-        
+        <mobiscroll.Form theme="ios-dark">
         <mobiscroll.FormGroup>
-            <mobiscroll.FormGroupTitle>Proactivity Mode Settings</mobiscroll.FormGroupTitle>
-            <mobiscroll.Switch theme="ios" value={this.state.switch1} onChange={this.onChange1}>
+            <h1>Proactivity Mode Settings</h1>
+            <mobiscroll.Switch theme="ios" value={this.state.mummy} onChange={this.onChange1}>
                 Mummy mode
                 <span className="mbsc-desc label-width">This mode turns on Mummy's nagging. Virtual mummy will remind you what you should or should not do to maintain good health.</span>
             </mobiscroll.Switch>
-            <mobiscroll.Switch theme="ios" value={this.state.switch2} onChange={this.onChange2}>
+            <mobiscroll.Switch theme="ios" value={this.state.bf} onChange={this.onChange2}>
                 Boyfriend mode
                 <span className="mbsc-desc label-width">This mode turns on the sensitive new age guy 'SNAG' and asks after you before/during the period.</span>
             </mobiscroll.Switch>
-            <mobiscroll.Switch theme="ios" value={this.state.switch1} onChange={this.onChange1}>
+            <mobiscroll.Switch theme="ios" value={this.state.husband} onChange={this.onChange3}>
                 Husband mode
                 <span className="mbsc-desc label-width">This mode puts up the 'DND' signal and informs your partner to be more mindful.</span>
             </mobiscroll.Switch>
         </mobiscroll.FormGroup>
+        </mobiscroll.Form>
       </div>
     )
   }
